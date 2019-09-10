4c933ca8a570aae9479b75828fc1c5f6035b5958
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-09-10 15:38:53 +0900

    first commit

--- WebView01/ViewController.swift
+++ WebView01/ViewController.swift
@@ -7,13 +7,63 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate, WKNavigationDelegate {
+    
+    @IBOutlet weak var searchbar: UISearchBar!
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var btnBack: UIBarButtonItem!
+    @IBOutlet weak var btnRefresh: UIBarButtonItem!
+    @IBOutlet weak var btnForward: UIBarButtonItem!
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchbar.delegate = self
+        webView.navigationDelegate = self
+        indicator.isHidden = true
+        
+        let request = URLRequest(url: URL(string: "https://www.google.com")!)
+        webView.load(request)
     }
+    
+    public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request = URLRequest(url: URL(string: searchbar.text!)!)
+        webView.load(request)
+        self.view.endEditing(true)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        btnBack.isEnabled = webView.canGoBack
+        btnForward.isEnabled = webView.canGoForward
+        
+        indicator.stopAnimating()
+        indicator.isHidden = true
+        searchbar.text = webView.url?.absoluteString
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        indicator.isHidden = false
+        indicator.startAnimating()
+    }
+    
+    
+    @IBAction func back(_ sender: Any) {
+        webView.goBack()
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+        webView.reload()
+    }
+    
+    @IBAction func forward(_ sender: Any) {
+        webView.goForward()
+    }
+    
 
 
 }
